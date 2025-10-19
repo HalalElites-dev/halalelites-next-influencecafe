c915e25daed4dbc0a804d31dcdbf7f788e5a6fb3
IbrahimShaikhli <ibrahim.alshaikhli@outlook.com>
2025-10-19 21:21:23 +0400

    remove line

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,6 @@ import { Roboto, Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider"
 import "./globals.css";
 
-<link rel="preload" as="image" href="/text2.webp" fetchPriority="high" />
 
 
 const roboto = Roboto({
